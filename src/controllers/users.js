503e6a8fc483bfba5a40c4b53dfa525113e15a56
blackbox <dzzirima@gmail.com>
2022-05-11 17:57:59 +0200

    feat :login done

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,36 +1,64 @@
-
 /**this is the controller function for all the user creation */
 
-import { Users } from "../models/index.js"
+import { Users } from "../models/index.js";
 
-let createUser = async (userOpts) =>{
+let createUser = async (userOpts) => {
+  if (!userOpts.username) {
+    throw new Error("Did not suppy username");
+  }
 
-    if(!userOpts.username){
-        throw new Error('Did not suppy username')
+  if (!userOpts.email) {
+    throw new Error("Did not suppy email");
+  }
+
+  if (!userOpts.password) {
+    throw new Error("Did not suppy password");
+  }
+
+  try {
+    const user = await Users.create({
+      ...userOpts,
+    });
+
+    if (!user) {
+      throw new Error("Error creating user");
+    }
+
+    return user;
+  } catch (error) {
+    console.log(error);
+  }
+};
+/**function to login the user */
+let verifyUser = async (userOpts) => {
+
+ 
+
+    if (!userOpts.email) {
+      throw new Error("Did not suppy email");
+    }
+
+    if (!userOpts.password) {
+      throw new Error("Did not suppy password");
+    }
+
+    const user = await Users.findOne({
+      where: {
+        email: userOpts.email,
+      },
+    });
+
+    if (!user) {
+      throw new Error("No user with given  email address");
+    }
+
+    if (user.passord != userOpts.password) {
+      throw new Error("Password does not match !!");
     }
     
-    if(!userOpts.email){
-        throw new Error('Did not suppy email')
-    }
-    
-    if(!userOpts.password){
-        throw new Error('Did not suppy password')
-    }  
 
-    try {
-        const user = await Users.create({
-            ...userOpts
-        })
+    return user;
+  
+};
 
-        if(!user){
-            throw new Error("Error creating user")
-        }
-        
-        return user
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export {createUser}
+export { createUser, verifyUser };
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUser } from "../../controllers/users.js";
+import { createUser,verifyUser } from "../../controllers/users.js";
 
 const route = Router()
 route.get('/',(req,res)=>{
@@ -25,6 +25,22 @@ route.post('/',async(req,res) =>{
     res.status(200).json({
         createdUser
     })
+})
+
+route.post('/login',async(req,res)=>{
+    
+    try {
+        const verifiedUser = await verifyUser(req.body.user)
+        res.status(200).json(verifiedUser)
+        
+    } catch (error) {
+        return res.status(403).json({
+            errors:{
+                body:[error.message]
+            }
+        })
+        
+    }
 })
 
 
