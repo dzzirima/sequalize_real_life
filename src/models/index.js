6cca9ae058a142f360b1ac70cb0395e7ef785a2b
blackbox <dzzirima@gmail.com>
2022-05-11 15:58:28 +0200

    feat:models and relationships done

--- src/models/index.js
+++ src/models/index.js
@@ -1,57 +1,99 @@
 /**this file form the bases of the configuration of the database */
 import { DataTypes, Sequelize } from "sequelize";
 
-const db = new Sequelize('realworld_api','postgres','1311',{
-    host:'localhost',
-    dialect:'postgres'
-})
+const db = new Sequelize("realworld_api", "postgres", "1311", {
+  host: "localhost",
+  dialect: "postgres",
+});
 
-
-const User = db.define('User',{
-    email:{
-        type:DataTypes.STRING,
-        validate:{
-            isEmail:true
-        },
-        unique:true,
-        allowNull:false
+const Users = db.define("User", {
+  email: {
+    type: DataTypes.STRING,
+    validate: {
+      isEmail: true,
     },
-    username:{
-        type:DataTypes.STRING,
-        primaryKey:true
+    unique: true,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+  },
+  bio: DataTypes.STRING,
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    validate: {
+      isUrl: true,
     },
-    bio:DataTypes.STRING,
-    image:{
-        type:DataTypes.STRING,
-        allowNull:true,
-        validate:{
-            isUrl:true
-        }
-    }
-})
+  },
+});
 
-const comment = db.define("Comment", {
-    id:{
-        type:DataTypes.INTEGER,
-        autoIncrement:true,
-        primaryKey:true
-
+const Comments = db.define(
+  "Comment",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    body:{
-        type:DataTypes.STRING,
-        validate:{
-            isNull:false
-        }
+    body: {
+      type: DataTypes.STRING,
+      validate: {
+        isNull: false,
+      },
     },
-    author:{
-        type:DataTypes.STRING,
-        validate:{
-            isNull:false
-        }
-    }
+    author: {
+      type: DataTypes.STRING,
+      validate: {
+        isNull: false,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-},{
-    timestamps:true
-})
+const Articles = db.define("article", {
+  slug: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING(50),
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING(100),
+  },
+  body: Sequelize.STRING,
+});
+const Tags = db.define("tag", {
+  name: {
+    type: Sequelize.STRING,
+    primaryKey: true,
+  },
+});
 
-export default db
+/**defining the relationships */
+Comments.belongsTo(Articles)
+Articles.hasMany(Comments)
+
+Comments.belongsTo(Users ,{as:"author"})
+Users.hasMany(Comments,{as:"author"})
+
+Articles.belongsTo(Users,{as:'author'})
+Users.hasMany(Articles,{as:'author'})
+
+
+/**many to many relationship */
+Articles.belongsToMany(Users,{through:'favourites'})
+Users.belongsToMany(Articles,{through:'favourites'}) 
+
+Articles.belongsToMany(Tags,{through:'article_tags'})
+Tags.belongsToMany(Articles,{through:'article_tags'})
+
+
+
+export {db,Users,Articles,Comments,Tags}
